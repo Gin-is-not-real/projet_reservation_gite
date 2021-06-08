9ec6464a6593b084a886edb369e0ba3b77685aa8
Alice58000 <finot.alice@gmail.com>
2021-06-08 15:30:11 +0200

    ajout classe hebergement

--- Hebergement.php
+++ Hebergement.php
@@ -5,7 +5,7 @@ class Hebergement {
     public $intitule;
     public $categorie;
     public $description;
-    public $photo;
+    public $photo;                                                                             
     public $nb_lits;
     public $nb_sdb;
     public $localisation;
@@ -22,5 +22,77 @@ class Hebergement {
         $this->localisation = $localisation;
         $this->prix = $prix;
         $this->disponibilite = $disponibilite;
+    }
+
+    public function getId() {
+        return $this->id;
+
+    }
+    public function getIntitule() {
+        return $this->intitule;
+    }
+
+    public function getCategorie() {
+        return $this->categorie;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function getNbLits() {
+        return $this->nb_lits;
+    }
+        
+    public function getNbSdb() {
+        return $this->nb_sdb;
+    }
+    
+    public function getLocalisation() {
+        return $this->localisation;
+    }
+
+    public function getPrix() {
+        return $this->prix;
+    }
+
+    public function getDisponibilite() {
+        return $this->disponibilite;
+    }
+
+    public function setId($id) {
+        $this->id=$id;
+    }
+
+    public function setIntitule($intitule) {
+        $this->intitule=$intitule;
+    }
+
+    public function setCategorie($categorie) {
+        $this->categorie=$categorie;
+    }
+
+    public function setDescription($description) {
+        $this->description=$description;
+    }
+
+    public function setNbLits($nb_lits) {
+        $this->nb_lits=$nb_lits;
+    }
+
+    public function setNbSdb($nb_sdb) {
+        $this->nb_sdb=$nb_sdb;
+    }
+
+    public function setLocalisation($localisation) {
+        $this->localisation=$localisation;
+    }
+
+    public function setPrix($prix) {
+        $this->prix=$prix;
+    }
+
+    public function setDisponibilite($disponibilite) {
+        $this->disponibilite=$disponibilite;
     }
 }
