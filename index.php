ff3aac0a812151abbe69e547d9990347afeadd6e
Alice58000 <finot.alice@gmail.com>
2021-06-10 11:50:11 +0200

    add reservation manager

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $GLOBALS["username"]= "root";
 $GLOBALS["password"]= '';
 $GLOBALS["basename"]= "projet_gites";
 $GLOBALS["tablename"]= "hebergements";
+$GLOBALS["tablenameresa"]= "reservations";
 
 $manager= new HebergementManager($GLOBALS["hostname"] , $GLOBALS["username"] , $GLOBALS["password"] , $GLOBALS["basename"] , $GLOBALS["tablename"]);
 
@@ -13,16 +14,53 @@ $manager= new HebergementManager($GLOBALS["hostname"] , $GLOBALS["username"] , $
 echo $manager->getTablename();
 
  
-//  $intitule= "titre";
-//  $categorie= "maison";
-//  $description= "caca";
-//  $photo= "...";                                                                             
-//  $nbLits= 2;
-//  $nbSdb= 2;
-//  $localisation="nevers";
-//  $prix= 10;
+ $intitule= "titre";
+ $categorie= "maison";
+ $description= "caca";
+ $photo= "...";                                                                             
+ $nbLits= 2;
+ $nbSdb= 2;
+ $localisation="nevers";
+ $prix= 10;
  $disponibilite=true;
 
-//  $manager->addHebergement($intitule, $categorie, $description, $photo, $nbLits, $nbSdb, $localisation, $prix, $disponibilite);
-//  $manager->deleteHebergement(2);
-  $manager->updateHebergement(2);
+ $manager->addHebergement($intitule, $categorie, $description, $photo, $nbLits, $nbSdb, $localisation, $prix, $disponibilite);
+
+ $manager->deleteHebergement(2);
+
+$_POST['intitule']="titre";
+$_POST['categorie']="maison";
+$_POST['description']="nouvelle description";
+$_POST['photo']="photo";
+$_POST['nbLits']=2;
+$_POST['nbSdb']=2;
+$_POST['localisation']="nevers";
+$_POST['prix']=10;
+$_POST['disponibilite']=true;
+
+
+$manager->updateHebergement(2);
+
+$manager= new ReservationsManager($GLOBALS["hostname"] , $GLOBALS["username"] , $GLOBALS["password"] , $GLOBALS["basename"] , $GLOBALS["tablenameresa"]);
+
+
+echo $manager->getTablenameresa();
+
+ 
+ $idHebergement=3;
+ $dateOccupation= "2021-06-15";
+ $dateLiberation= "2021-06-30";                                                                             
+ 
+
+ $manager->addReservations($idHebergement, $dateOccupation, $dateLiberation);
+ 
+ $manager->deleteReservations(6);
+
+$_POST['id_hebergement']=3;
+$_POST['date_occupation']="2021-06-20";
+$_POST['date_liberation']="2021-06-30";
+
+
+
+$manager->updateReservations(10);
+
