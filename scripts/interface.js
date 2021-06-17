d7496808a0c87f02fb75ad2895d73accc1f7b04a
Gin-is-not-real <ninapariat@live.fr>
2021-06-17 14:12:57 +0200

    problem with the class switch for the focus of the selected card resolved

--- scripts/interface.js
+++ scripts/interface.js
@@ -1,5 +1,5 @@
 let cards = document.querySelectorAll('.card');
-let btnSeeMore = document.querySelectorAll('.btn-seemore');
+// let btnSeeMore = document.querySelectorAll('.btn-seemore');
 
 // btnSeeMore.forEach(btn => {
 //     btn.addEventListener('click', function() {
@@ -14,7 +14,7 @@ let btnSeeMore = document.querySelectorAll('.btn-seemore');
 
 let lastSelectedCard;
 cards.forEach(card => {
-    // card.classList.add('unfocused-card');
+    card.classList.add('unfocused-card');
 
     card.addEventListener('click', function() {
         focusOnTheSelectedCard(card);
@@ -23,29 +23,33 @@ cards.forEach(card => {
 
 //focus the selected card
 function focusOnTheSelectedCard(card) {
-    if(lastSelectedCard != undefined) {
+    if(lastSelectedCard != undefined && lastSelectedCard != card) {
         lastSelectedCard.classList.remove('focused-card');
-        // lastSelectedCard.classList.add('unfocused-card');
+        lastSelectedCard.classList.add('unfocused-card');
+        console.log('lastSelectedCard != undefined');
     }
-    // card.classList.remove('unfocused-card');
-    card.classList.add('focused-card');
-    // card.style.position = 'fixed';
+
+    if(lastSelectedCard == card) {
+        console.log('lastSelectedCard == card');
+    }
+    else {
+        card.classList.remove('unfocused-card');
+        card.classList.add('focused-card');
+        console.log('else');
+    }
     lastSelectedCard = card;
 }
 
 let btnsClose = document.querySelectorAll('#card-close');
 btnsClose.forEach(btn => {
     btn.addEventListener('click', function() {
-        // console.log('lastSelectedCard', lastSelectedCard);
-
         lastSelectedCard.classList.remove('focused-card');
-        // lastSelectedCard.classList.add('unfocused-card');
-        // lastSelectedCard.style.position = 'static';
+        lastSelectedCard.classList.add('unfocused-card');
 
-        console.log('lastSelectedCard', lastSelectedCard);
-
-        // lastSelectedCard = undefined;
-
+        window.setTimeout(function() {
+            lastSelectedCard = undefined;
+            console.log('close', lastSelectedCard);
+        }, 300);
     })
 })
 
